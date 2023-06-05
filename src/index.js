d035cd53178b18dd44e011691ccee05427d02985
hainuo <1093007705@qq.com>
2023-06-05 14:50:13 +0800

    feat: 函数组件的渲染原理

--- src/index.js
+++ src/index.js
@@ -1,25 +1,14 @@
-// import React, { Children } from "react";
-// import ReactDOM from "react-dom/client";
+import React, { Children } from "react";
+import ReactDOM from "react-dom/client";
+import DemoOne from "./views/DemoOne";
 import "./index.css";
-import { render, createElement } from "./utils/jsxHandler";
 import reportWebVitals from "./reportWebVitals";
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//     <div>
-//       <h2 className="class"></h2>
-//       <button style={{color:'red'}}>nihao</button>
-//     </div>
-// );
-
-const vDom = createElement(
-  "div",
-  null,
-  createElement("h2", { className: "class" }, "hoibao"),
-  createElement("button", { style:{
-    color: 'red'
-  } }, 10),
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <DemoOne/>
 );
-render(vDom, document.getElementById("root"));
+
+
 
 reportWebVitals();
